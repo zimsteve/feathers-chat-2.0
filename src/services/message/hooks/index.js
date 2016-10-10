301a91c5333d30727fc64dc7c16e7ba2ad6c579f
Hikurangi & Steve Sharp <pair+hikurangi.zimsteve@devbootcamp.com>
2016-10-10 16:19:45 +1300

    NAILED IT

--- src/services/message/hooks/index.js
+++ src/services/message/hooks/index.js
@@ -1,9 +1,16 @@
 'use strict';
 
-const process = require('./process');
+const restrictToSender = require('./restrict-to-sender');
 
-const globalHooks = require('../../../hooks');
 const auth = require('feathers-authentication').hooks;
+const hooks = require('feathers-hooks');
+
+const process = require('./process');
+const globalHooks = require('../../../hooks');
+const populateSender = hooks.populate('sentBy', {
+  service: 'users',
+  field: 'userId'
+});
 
 exports.before = {
   all: [
@@ -13,17 +20,17 @@ exports.before = {
   ],
   find: [],
   get: [],
-  create: [],
-  update: [],
-  patch: [],
-  remove: []
+  create: [process()],
+  update: [hooks.remove('sentBy'), restrictToSender()],
+  patch: [hooks.remove('sentBy'), restrictToSender()],
+  remove: [restrictToSender()]
 };
 
 exports.after = {
   all: [],
-  find: [],
-  get: [],
-  create: [],
+  find: [populateSender],
+  get: [populateSender],
+  create: [populateSender],
   update: [],
   patch: [],
   remove: []
