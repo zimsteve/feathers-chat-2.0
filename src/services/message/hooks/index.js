96337f5e191d8f41c2e51571d52c3e2bd120f9c4
Hikurangi & Steve Sharp <pair+hikurangi.zimsteve@devbootcamp.com>
2016-10-10 15:48:27 +1300

    before the after hooks

--- src/services/message/hooks/index.js
+++ src/services/message/hooks/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
-const globalHooks = require('../../../hooks');
-const hooks = require('feathers-hooks');
+const process = require('./process');
 
+const globalHooks = require('../../../hooks');
+const auth = require('feathers-authentication').hooks;
 
 exports.before = {
-  all: [],
+  all: [
+    auth.verifyToken(),
+    auth.populateUser(),
+    auth.restrictToAuthenticated()
+  ],
   find: [],
   get: [],
   create: [],
